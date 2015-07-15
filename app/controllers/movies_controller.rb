1b65723e12dd4d2e20eb4ac7cc0ac3ee79ae6936
AnnushkaV <verezub_anna@mail.ru>
2015-07-15 23:11:33 +0300

    add retings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,15 +2,18 @@ class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /movies
-  # GET /movies.json
   def index
     @movies = Movie.all
   end
 
-  # GET /movies/1
-  # GET /movies/1.json
   def show
+    @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).round(2)
+    end
   end
 
   # GET /movies/new
